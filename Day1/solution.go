25443978c53fc20231e881135d8d7f74af5939b0
travertischio <traver@capsule8.com>
2018-12-05 17:24:52 -0500

    Complete day 1 part 2

--- Day1/solution.go
+++ Day1/solution.go
@@ -12,6 +12,9 @@ import (
 func main() {
 	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 	frequency := int64(0)
+	list := make([]int64, 0)
+	frequencies := make(map[int64]bool)
+	frequencies[0] = true
 
 	for {
 		nRead := readLine(reader)
@@ -21,10 +24,25 @@ func main() {
 
 		n, err := strconv.ParseInt(nRead, 10, 64)
 		checkError(err)
+		list = append(list, n)
 		frequency += n
+		if frequencies[frequency] {
+			fmt.Println(frequency)
+			return
+		}
+		frequencies[frequency] = true
 	}
 
-	fmt.Println(frequency)
+	for {
+		for _, listN := range list {
+			frequency += int64(listN)
+			if frequencies[frequency] {
+				fmt.Println(frequency)
+				return
+			}
+			frequencies[frequency] = true
+		}
+	}
 }
 
 func readLine(reader *bufio.Reader) string {
