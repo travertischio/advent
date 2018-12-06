8f5c5352298593aecb69ba699cf47a194c4fccc3
travertischio <traver@capsule8.com>
2018-12-05 20:41:55 -0500

    Complete day3 part 2

--- Day3/solution.go
+++ Day3/solution.go
@@ -12,15 +12,18 @@ import (
 func main() {
 	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 	fabric := make(map[int]map[int]int)
-	overlaps := 0
+	overlaps := make(map[int]bool)
+	claimTotal := 0
 
 	for {
 		claim := readLine(reader)
 		if claim == "" {
 			break
 		}
+		claimTotal++
 
 		claimArr := strings.Split(claim, " ")
+
 		startArr := strings.Split(claimArr[2], ",")
 		startY := startArr[1][:len(startArr[1])-1]
 		yTemp, err := strconv.ParseInt(startY, 10, 64)
@@ -39,19 +42,31 @@ func main() {
 		checkError(err)
 		h := int(hTemp)
 
+		cTemp, err := strconv.ParseInt(claimArr[0][1:], 10, 64)
+		checkError(err)
+		claimID := int(cTemp)
+
 		for i := 0; i < w; i++ {
 			for j := 0; j < h; j++ {
 				if fabric[x+i] == nil {
 					fabric[x+i] = make(map[int]int)
 				}
-				fabric[x+i][y+j]++
-				if fabric[x+i][y+j] == 2 {
-					overlaps++
+
+				if fabric[x+i][y+j] > 0 {
+					overlaps[claimID] = true
+					overlaps[fabric[x+i][y+j]] = true
 				}
+				fabric[x+i][y+j] = claimID
 			}
 		}
 	}
-	fmt.Println(overlaps)
+
+	for i := 1; i <= claimTotal; i++ {
+		if overlaps[i] == false {
+			fmt.Println(i)
+			break
+		}
+	}
 }
 
 func readLine(reader *bufio.Reader) string {
