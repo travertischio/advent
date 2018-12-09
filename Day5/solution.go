35177f076a84a34746d7fc36007ec0e29b753585
travertischio <traver@capsule8.com>
2018-12-09 09:40:32 -0500

    Complete Day 5 Part 2

--- Day5/solution.go
+++ Day5/solution.go
@@ -11,20 +11,31 @@ import (
 func main() {
 	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 
-	polymer := []byte(readLine(reader))
+	polymer := []rune(readLine(reader))
 
-	for i := 0; i < len(polymer)-1; i++ {
-		if abs(int(polymer[i])-int(polymer[i+1])) == 32 {
-			polymer = append(polymer[:i], polymer[i+2:]...)
-			if i == 0 {
-				i = -1
-			} else {
-				i -= 2
+	shortest := len(polymer)
+	for j := 'A'; j <= 'Z'; j++ {
+		thisPoly := polymer
+
+		thisPoly = []rune(strings.Replace(string(thisPoly), string(j), "", -1))
+		thisPoly = []rune(strings.Replace(string(thisPoly), string(rune(j+32)), "", -1))
+
+		for i := 0; i < len(thisPoly)-1; i++ {
+			if abs(int(thisPoly[i])-int(thisPoly[i+1])) == 32 {
+				thisPoly = append(thisPoly[:i], thisPoly[i+2:]...)
+				if i == 0 {
+					i = -1
+				} else {
+					i -= 2
+				}
+
 			}
-
+		}
+		if len(thisPoly) < shortest {
+			shortest = len(thisPoly)
 		}
 	}
-	fmt.Println(len(polymer))
+	fmt.Println(shortest)
 }
 
 func readLine(reader *bufio.Reader) string {
